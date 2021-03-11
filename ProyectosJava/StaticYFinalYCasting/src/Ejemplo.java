f9f552bde13193e86efdac83381ccb99b7e18e94
cracka07 <39442992+cracka07@users.noreply.github.com>
2021-03-11 13:15:42 -0300

    Add files via upload
    Commit succesfull

---
+++ ProyectosJava/StaticYFinalYCasting/src/Ejemplo.java
@@ -0,0 +1,11 @@
+
+public class Ejemplo {
+   
+   final int numero=37;
+   
+   static int valor=32;
+   
+   public Ejemplo() {
+//	   numero=32; no se puede modificar
+   }
+}
---
+++ ProyectosJava/StaticYFinalYCasting/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+  
+	public static void main(String args[]) {
+		
+		//Accediendo al valor static
+		System.out.println(Ejemplo.valor);
+		
+		double num=3222.800;
+		int num1=(int)num;
+		
+		System.out.println(num1);
+	}
+
+}
